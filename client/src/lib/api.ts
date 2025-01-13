732b233a4087cced7ee4687452a8c3c8200de762
Ilyes Bechoual <112644383+Bugz-gg@users.noreply.github.com>
2025-01-13 19:06:21 +0100

    Update api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -14,7 +14,7 @@ export type filterType = 'allShocks' | 'userShocks' | 'userRoutes';
 
 export type apiRoute = 'importSensorData' | 'user/me' | filterType;
 
-export const api = 'http://20.105.232.27:8000';
+export const api = 'http://'${{ secrets.IP }}':8000';
 
 // const getTokenFromLocal = () => {
 //   const token: Token = {
